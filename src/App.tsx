180ca0580c0a6a888ad7f02b7fcb5e1d9208d443
Paulo Henrique <pauloh.f.oliveira1@gmail.com>
2022-10-27 09:32:18 -0300

    ci: edit yml file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { Logo } from './Logo';
 import './styles/global.css';
+import { Heading } from './components/Heading'
 
 export function App() {
 
   return (
-    <>
-      <h1 className=''>Oi</h1>
-    </>
+    <div className='w-screen h-screen bg-gray-900 flex items-center justify-center text-gray-100'>
+      <Logo />
+      <Heading size='lg'>
+        Ignite Lab
+      </Heading>
+    </div>
   )
 }
 
